632315ffd2022aa8c0e0eef254a58cf87db11c91
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-12-08 17:49:46 +0100

    fixed embedFiles for ghc >8.8

--- src/FileEmbed.hs
+++ src/FileEmbed.hs
@@ -1,28 +1,31 @@
-{-# LANGUAGE TemplateHaskell ,CPP #-}
-module FileEmbed(asTextLibrary,anyMatching,embedFiles) where
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module FileEmbed (asTextLibrary, anyMatching, embedFiles) where
+
+import Data.Bifunctor
 import qualified Data.ByteString as B
 import Data.FileEmbed
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Text (Text)
 import Language.Haskell.TH.Syntax
 import Text.Regex
-import Data.Text(Text)
-import Data.Maybe
-import qualified Data.Map as M
-import Data.Bifunctor
-import ZM(convert)
+import ZM (convert)
 
 anyMatching :: [String] -> String -> Bool
 anyMatching rexs =
-    let ps = map (\r-> isJust . matchRegex (mkRegex r)) rexs
-    in \s -> any  ($ s) ps
+    let ps = map (\r -> isJust . matchRegex (mkRegex r)) rexs
+     in \s -> any ($ s) ps
 
--- Read as UTF8 files    
-asTextLibrary :: [(FilePath,B.ByteString)] -> M.Map FilePath Text   
+-- Read as UTF8 files
+asTextLibrary :: [(FilePath, B.ByteString)] -> M.Map FilePath Text
 asTextLibrary = M.fromList . map (second convert)
 
 -- Code copied/adapted from package file-embed, module Data.FileEmbed
 
 {- | Embed a directory recursively in your source code.
-   
+
  > import qualified Data.ByteString
  >
  > myDir :: [(FilePath, Data.ByteString.ByteString)]
@@ -31,19 +34,22 @@ asTextLibrary = M.fromList . map (second convert)
 
  > library :: M.Map FilePath T.Text
  > library = asTextLibrary $ $(embedFiles "quid2-ts" (anyMatching ["core/[^.]+\\.ts","core\\.ts","api\\.ts"]))
- -}
-
-embedFiles :: FilePath -> (FilePath -> Bool) ->  Q Exp
+-}
+embedFiles :: FilePath -> (FilePath -> Bool) -> Q Exp
 embedFiles fp predicate = do
-    typ <- [t| [(FilePath, B.ByteString)] |]
+    typ <- [t|[(FilePath, B.ByteString)]|]
     e <- ListE <$> (runIO (filter (predicate . fst) <$> getDir fp) >>= mapM (pairToExp fp))
     return $ SigE e typ
 
 pairToExp :: FilePath -> (FilePath, B.ByteString) -> Q Exp
 pairToExp _root (path, bs) = do
 #if MIN_VERSION_template_haskell(2,7,0)
-        qAddDependentFile $ _root ++ '/' : path
+    qAddDependentFile $ _root ++ '/' : path
 #endif
-        exp' <- bsToExp bs
-        return $! TupE [LitE $ StringL path, exp']
-
+    exp' <- bsToExp bs
+    return
+        $! TupE
+#if MIN_VERSION_template_haskell(2,16,0)
+      $ map Just
+#endif
+            [LitE $ StringL path, exp']
